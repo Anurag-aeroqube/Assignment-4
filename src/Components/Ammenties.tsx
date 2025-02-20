e0eb9352a27e5c7cca982c50659a9aa644e1e454
Anurag-aeroqube <anuragm@aeroqube.com>
2025-02-20 11:05:17 +0530

    Added card

--- src/Components/Ammenties.tsx
+++ src/Components/Ammenties.tsx
@@ -1,22 +1,20 @@
 import React from 'react'
-import bulb from '../assets/bulb.svg'
-import item1 from '../assets/Group 3037.svg'
-import item2 from '../assets/Group 3038.svg'
-import item3 from '../assets/Group 3200.svg'
-import item4 from '../assets/Group 3038.svg'
-import item5 from '../assets/Group 3037.svg'
-import item6 from '../assets/Group 3200.svg'
-import item7 from '../assets/Group 3038.svg'
+
+import image1 from '../assets/Group 3206.svg'
+import image2 from '../assets/Group 3204.svg'
+import image3 from '../assets/Group 3205.svg'
+
 const Amenities = () => {
   return (
-    <section className='w-full grid grid-cols-3 gap-10 mt-4 mt-[60px]'>
-        <figure className=''><img src={item1} alt="" className='object-contain w-full'/></figure>
-        <figure><img src={item2} alt="" className='object-contain w-full'/></figure>
-        <figure><img src={item3} alt="" className='object-contain w-full'/></figure>
-        <figure><img src={item4} alt="" className='object-contain w-full'/></figure>
-        <figure><img src={item5} alt="" className='object-contain w-full'/></figure>
-        <figure><img src={item6} alt="" className='object-contain w-full'/></figure>
-        <figure><img src={item7} alt="" className='object-contain w-full'/></figure>
+    <section className='w-full grid grid-cols-3 space-y-10 gap-x-16 mt-[56px]'>
+        <figure className=''><img src={image1} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image2} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image3} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image2} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image3} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image2} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image3} alt="" className='object-contain w-full'/></figure>
+        <figure><img src={image3} alt="" className='object-contain w-full'/></figure>
     </section>
   )
 }
--- src/Components/VillasDetails.tsx
+++ src/Components/VillasDetails.tsx
@@ -28,21 +28,32 @@ const VillasDetails = () => {
               <VCrousel />
             </div>
             <div className='col-span-1 grid grid-cols-2 gap-7'>
-              {[...Array(8)].map((_, index) => (
-                <img key={index} src={gallery1} alt={`Villa ${index + 1}`} className='rounded-md shadow-lg' />
-              ))}
+              <img src={gallery1}></img>
+              <img src={gallery2}></img>
+              <img src={gallery2}></img>
+              <img src={gallery1}></img>
+              <img src={gallery1}></img>
+              <img src={gallery2}></img>
+              <img src={gallery2}></img>
+              <img src={gallery1}></img>
             </div>
           </section>
 
           <section className='grid grid-cols-3'>
             <div className='col-span-2'>
-              <div className='bg-white shadow-xl border border-gray-200 p-4 mt-4 rounded-md text-center'>
+              <div className='bg-[#ffffff] shadow-md shadow-[#00000011]/7 border border-gray-200 p-4 mt-4 rounded-md text-center'>
                 <div className='flex flex-row justify-between'>
-                  <p className='font-Nunito-Sans font-bold lg:text-[1.6rem] text-[0.7rem]'>{villa.title}</p>
+                  <p className='font-Nunito-Sans font-bold  text-[#3e3e3e] lg:text-[1.6rem] text-[0.7rem]'>{villa.title}</p>
                   <p className='text-[#ffa800] font-bold text-[1.6rem]'>{villa.price}</p>
                 </div>
-                <p className='text-[#aeaeae] font-Nunito-Sans text-[1.3rem] text-left py-3'><img src={location} className="inline" />{villa.address}</p>
-                <div className='flex flex-row space-x-14  mx-auto'>
+                <figure className='flex space-x-2 '>
+                <img src={location} className="w-[1.5%]" />
+                <p className='text-[#aeaeae] font-Nunito-Sans text-[1.3rem] text-left py-3'>{villa.address}</p>
+                  
+                </figure>
+               
+                <div className='flex flex-row gap-x-14  mx-auto'>
+                  
                   <p className='text-[#626262] font-Nunito-Sans font-semibold text-[1.3rem]'><img src={bathtub} className="inline" /> {villa.bath}</p>
                   <p className='text-[#626262] font-Nunito-Sans font-semibold text-[1.3rem]'><img src={room} className="inline" /> {villa.beds}</p>
                   <p className='text-[#626262] font-Nunito-Sans font-semibold text-[1.3rem]'><img src={house} className="inline" /> {villa.size}</p>
@@ -53,16 +64,16 @@ const VillasDetails = () => {
 
           <section className='h-15 flex justify-around items-center bg-[#f4f4f4] w-2/3 mt-10'>
             <h1
+              className={`cursor-pointer px-4 py-2 font-mulish font-bold text-[#181725] ${activeSection === 'layout' ? 'border-b-2 border-blue-500' : 'text-[#8a8a8a] font-mulish font-semibold'}`}
+              onClick={() => setActiveSection('layout')}
+            >
+               PROPERTY LAYOUT
+            </h1>
+            <h1
               className={`cursor-pointer px-4 py-2 font-mulish font-bold text-[#181725] ${activeSection === 'description' ? 'border-b-2 border-blue-500' : 'text-[#8a8a8a] font-mulish font-semibold'}`}
               onClick={() => setActiveSection('description')}
             >
               DESCRIPTION
-            </h1>
-            <h1
-              className={`cursor-pointer px-4 py-2 font-mulish font-bold text-[#181725] ${activeSection === 'layout' ? 'border-b-2 border-blue-500' : 'text-[#8a8a8a] font-mulish font-semibold'}`}
-              onClick={() => setActiveSection('layout')}
-            >
-              PROPERTY LAYOUT
             </h1>
             <h1
               className={`cursor-pointer px-4 py-2 font-mulish font-bold text-[#181725] ${activeSection === 'amenities' ? 'border-b-2 border-blue-500' : 'text-[#8a8a8a] font-mulish font-semibold'}`}
