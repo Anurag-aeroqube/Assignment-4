64a8edbc66fff99aa35772a85114e2e6e420cbf6
Anurag-aeroqube <anuragm@aeroqube.com>
2025-02-21 09:40:03 +0530

    Addind villas

--- src/Components/Partners.tsx
+++ src/Components/Partners.tsx
@@ -1,89 +1,79 @@
-import React, { useState } from 'react';
-import gallery1 from '../assets/gallery-1.png';
 
-const Partners = () => {
-  const [activeTab, setActiveTab] = useState('PROPERTY LAYOUT');
-
+ 
+import React from 'react'
+ 
+const PropertyLayout = () => {
   return (
-    <div className='w-full'>
-      <div className='w-[80%] mt-15 mx-auto'>
-        <section className='grid grid-cols-3 w-full mx-auto gap-6'>
-          <div className='col-span-2 '>
-            <img src={gallery1} className='w-full' alt='Gallery' />
-          </div>
-          <div className='col-span-1 grid grid-cols-2 gap-7'>
-            {[...Array(8)].map((_, index) => (
-              <img key={index} src={gallery1} alt={`Villa ${index + 1}`} className='rounded-md shadow-lg' />
-            ))}
-          </div>
-        </section>
-
-        <section className='grid grid-cols-3'>
-          <div className='col-span-2'>
-            <div className='bg-white shadow-xl border border-gray-200 p-4 mt-4 rounded-md text-center'>
-              <div>
-                <div className='flex flex-row justify-between'>
-                  <p className='font-Nunito-Sans font-bold lg:text-[1.6rem] text-[0.7rem]'>Hilltruck Valley</p>
-                  <p className='text-[#ffa800] font-bold text-[1.6rem]'>₹375,000</p>
-                </div>
-                <p className='text-[#aeaeae] font-Nunito-Sans text-[1.3rem] text-left py-3'>1187 Lake Forest City, Uttrakhand</p>
-              </div>
-              <div className='flex flex-row justify-between w-[60%]'>
-                <p className='text-[#626262] font-Nunito-Sans font-semibold  text-[1.3rem]'>2 Baths</p>
-                <p className='text-[#626262] font-Nunito-Sans font-semibold text-[1.3rem]'>4 Beds</p>
-                <p className='text-[#626262] font-Nunito-Sans font-semibold text-[1.3rem]'>2980 sqft</p>
-              </div>
-            </div>
-
-            <section className='mt-7'>
-              <nav className='bg-[#f4f4f4] flex flex-row justify-between w-full md:p-3'>
-                {['PROPERTY LAYOUT', 'DESCRIPTION', 'AMENITIES'].map((tab) => (
-                  <a
-                    key={tab}
-                    className={`cursor-pointer px-4 py-2 font-mulish font-bold text-[#181725] ${activeTab === tab ? '   border-b-2  border-blue-500' : '  font-mulish font-semibold text-[#8a8a8a] '}`}
-                    onClick={() => setActiveTab(tab)}
-                  >
-                    {tab}
-                  </a>
-                ))}
-              </nav>
-
-              {activeTab && (
-                <div className='p-4  mt-2 pl-3 rounded-md'>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-                  <p className='font-mulish text-[1.1rem] text-[#737b7d] mb-4'>
-                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                  </p>
-
-                  
-                </div>
-              )}
-            </section>
-          </div>
-        </section>
-      </div>
+    <div className='flex flex-col mt-[60px]'>
+    <h1 className='font-mulish font-bold text-left text-[24px] '>Description:</h1>
+ 
+    <ol className=' list-decimal text-[#737b7d] pl-4 mt-7'>
+        <li className='py-3 font-mulish text-left text-[18px] text-[#737b7d]'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
+ 
+        <li className='py-3 font-mulish text-left text-[18px] text-[#737b7d]'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
+ 
+        <li className='py-3 font-mulish text-left text-[18px] text-[#737b7d]'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
+ 
+        <li className='py-3 font-mulish text-left text-[18px] text-[#737b7d]'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
+ 
+        <li className='py-3 font-mulish text-left text-[18px] text-[#737b7d]'>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
+    </ol>
+ 
+    <section>
+    <article className='flex flex-col items-start justify-start w-full'>
+  <div className='text-left py-2'>
+    <span className='font-mulish font-bold text-[24px] text-[#373f41]'>Address : </span>
+    <p className='font-mulish font-semibold text-[20px] text-[#737b7d] inline'>
+      HOUSE NO.- 99, THAPLIYA MAEHRA , Vijraula, Naukukichiatal
+    </p>
+  </div>
+  <hr className="w-full border-t border-[#eaeaea]" />
+</article>
+ 
+<article className='flex flex-col items-start justify-start w-full'>
+  <div className='text-left py-2'>
+    <span className='font-mulish font-bold text-[24px] text-[#373f41]'>Contact No. : </span>
+    <p className='font-mulish font-semibold text-[20px] text-[#737b7d] inline'>
+     8747593736
+    </p>
+  </div>
+  <hr className="w-full border-t border-[#eaeaea]" />
+</article>
+ 
+<article className='flex flex-col items-start justify-start w-full'>
+  <div className='text-left py-2'>
+    <span className='font-mulish font-bold text-[24px] text-[#373f41]'>Beds : </span>
+    <p className='font-mulish font-semibold text-[20px] text-[#737b7d] inline'>
+    06 Beds
+    </p>
+  </div>
+  <hr className="w-full border-t border-[#eaeaea]" />
+</article>
+ 
+<article className='flex flex-col items-start justify-start w-full'>
+  <div className='text-left py-2'>
+    <span className='font-mulish font-bold text-[24px] text-[#373f41]'>Check In : </span>
+    <p className='font-mulish font-semibold text-[20px] text-[#737b7d] inline'>
+      08
+    </p>
+  </div>
+  <hr className="w-full border-t border-[#eaeaea]" />
+</article>
+ 
+ 
+<article className='flex flex-col items-start justify-start w-full'>
+  <div className='text-left py-2'>
+    <span className='font-mulish font-bold text-[24px] text-[#373f41]'>Check Out : </span>
+    <p className='font-mulish font-semibold text-[20px] text-[#737b7d] inline'>
+      12
+    </p>
+  </div>
+  <hr className="w-full border-t border-[#eaeaea]" />
+</article>
+    </section>
     </div>
-  );
-};
-
-export default Partners;
+   
+  )
+}
+ 
+export default PropertyLayout
--- src/Components/Villas.tsx
+++ src/Components/Villas.tsx
@@ -18,7 +18,7 @@ const Villas = () => {
             </h1>
           </article>
   
-          <div className='grid grid-cols-1 space-y-20  sm:grid-cols-2 lg:grid-cols-3 sm:space-y-35'>
+          <div className='grid grid-cols-1 space-y-20 gap-8 sm:grid-cols-2 lg:grid-cols-3 sm:space-y-35'>
   {villas.map((villa) => (
     <Villacard key={villa.id} villa={villa} />
   ))}
