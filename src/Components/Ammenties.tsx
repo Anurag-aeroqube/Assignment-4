83ab73f5a0ade57668ebd99c7e1fa94bc8be0ee3
Anurag-aeroqube <anuragm@aeroqube.com>
2025-02-24 11:14:18 +0530

    correct Ammenities

--- src/Components/Ammenties.tsx
+++ src/Components/Ammenties.tsx
@@ -8,10 +8,10 @@ interface AmenityProps {
 const Amenities: React.FC<AmenityProps> = ({ amenities }) => {
   return (
     <>
-<section className='w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-10'>
+<section className='w-[95%] mx-auto grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3  gap-10 mt-10 '>
   {
     amenities.map((item,index)=>(
-      <div key={index} className='h-[10rem] w-[10rem] md:h-[12.5rem] md:w-[12.5rem] bg-[#6d87cb] flex flex-col justify-center items-center rounded-sm gap-4 mx-auto'>
+      <div key={index} className='h-[9.5rem] w-[9.5rem] md:h-[12.5rem] md:w-[12.5rem] bg-[#6d87cb] flex flex-col justify-center items-center rounded-sm gap-4 mx-auto'>
       <img src={item.logo} alt="" />
       <p className='text-[#ffffff] font-navabarLinks text-[10px] sm:text-[14px] font-extrabold'>{item.title}</p>
     </div>
